967eeb39aaffa6ad87914f8fff2f42baed80b73e
hoangngotung22@gmail.com <hoangngoctung22@gmail.com>
2022-04-19 22:29:58 +0700

    first commit

---
+++ src/Baitapchuong3.java
@@ -0,0 +1,21 @@
+
+public class Baitapchuong3 {System.out.print("nhap ma sach: ");
+             masach = sc.nextLine();
+             System.out.print("nhap tensach: ");
+             tensach = sc.nextLine(); 
+             System.out.print("nhap tentacgia: ");
+             tentacgia = sc.nextLine(); 
+             System.out.print("nhap sotrang: ");
+             sotrang = sc.nextInt(); 
+             System.out.print("nhap loaisach: ");
+             loaisach = sc.next(); 
+             System.out.print("nhap nhaxuatban: ");
+             nhaxuatban = sc.next(); ;
+             System.out.print("nhap sotap: ");
+             sotap = sc.nextInt(); 
+             System.out.print("nhap gia: ");
+             gia = sc.nextInt(); 
+        }
+    }
+
+}
